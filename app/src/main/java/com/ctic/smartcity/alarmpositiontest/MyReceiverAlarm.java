1f1b64b4f99ba3fbee9300091d6138a262272b08
gio <vgiovanny.mondragon@uni.pe>
2017-11-03 09:24:18 -0500

    ready

--- app/src/main/java/com/ctic/smartcity/alarmpositiontest/MyReceiverAlarm.java
+++ app/src/main/java/com/ctic/smartcity/alarmpositiontest/MyReceiverAlarm.java
@@ -12,9 +12,8 @@ import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.util.Log;
 
-public class MyReceiverAlarm extends BroadcastReceiver{
+public class MyReceiverAlarm extends BroadcastReceiver implements LocationListener{
     private LocationManager locationManager;
-    LocationListener locationListener;
     // The minimum distance to change Updates in meters
     private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 10; // 10 meters
 
@@ -24,28 +23,41 @@ public class MyReceiverAlarm extends BroadcastReceiver{
     @Override
     public void onReceive(Context context, Intent intent) {
 
-        Log.i("GIODEBUG_", "Alarm set. Aca mando ultima posicion");
         locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
         boolean // getting GPS status
                 isGPSEnabled = locationManager
                 .isProviderEnabled(LocationManager.GPS_PROVIDER);
-
-        // getting network status
-        boolean isNetworkEnabled = locationManager
-                .isProviderEnabled(LocationManager.NETWORK_PROVIDER);
-        boolean isSomething = locationManager
-                .isProviderEnabled(LocationManager.PASSIVE_PROVIDER);
         if (ActivityCompat.checkSelfPermission(context,
                 Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
                 ActivityCompat.checkSelfPermission(context,
                         Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             return;
         }
-        if (isGPSEnabled) {///This is enough,
-            Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-            if(location!=null)/// and if user is != vacio
-                Log.d("GIODEBUG_", "GPS! "+location.getLatitude()+"_"+location.getLongitude());
+        if (isGPSEnabled) {
+            locationManager.requestLocationUpdates(LocationManager.
+                    GPS_PROVIDER,MIN_TIME_BW_UPDATES,
+                    MIN_DISTANCE_CHANGE_FOR_UPDATES,this);
+
         }
     }
 
+    @Override
+    public void onLocationChanged(Location location) {
+        locationManager.removeUpdates(this);
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
+    }
 }
